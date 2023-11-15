0f7c0f6f7861bb570222f48007a70df4b6b8f203
Luiz Fernando Becher de Araujo <luiz.becher.araujo@gmail.com>
2023-11-15 18:19:49 -0300

    changing mount point

--- install.sh
+++ install.sh
@@ -14,39 +14,39 @@ parted -s $STORAGE_DEVICE set 1 boot on
 export ROOT_PARTUUID=$( blkid -o value -s PARTUUID )
 
 mkfs.ext4 $ROOT
-mkdir /mnt/sd
-mount $ROOT /mnt/sd
+mkdir /mnt/debian
+mount $ROOT /mnt/debian
 
-debootstrap --arch=armhf --foreign bookworm /mnt/sd http://deb.debian.org/debian
+debootstrap --arch=armhf --foreign bookworm /mnt/debian http://deb.debian.org/debian
 
-cp -v -r modules/* /mnt/sd/lib/modules/
-cp -v -r boot/* /mnt/sd/boot/
+cp -v -r modules/* /mnt/debian/lib/modules/
+cp -v -r boot/* /mnt/debian/boot/
 
-echo "  APPEND earlyprintk root=PARTUUID=$ROOT_PARTUUID rootwait rootfstype=ext4 init=/sbin/init loglevel=0 fsck.repair=yes video=HDMI-A-1:1280x720" >> /mnt/sd/boot/extlinux/extlinux.conf
+echo "  APPEND earlyprintk root=PARTUUID=$ROOT_PARTUUID rootwait rootfstype=ext4 init=/sbin/init loglevel=0 fsck.repair=yes video=HDMI-A-1:1280x720" >> /mnt/debian/boot/extlinux/extlinux.conf
 
-cp -v /etc/resolv.conf /mnt/sd/etc/resolv.conf
-cp -v sources.list /mnt/sd/etc/apt/sources.list
+cp -v /etc/resolv.conf /mnt/debian/etc/resolv.conf
+cp -v sources.list /mnt/debian/etc/apt/sources.list
 
-chroot /mnt/sd /debootstrap/debootstrap --second-stage
+chroot /mnt/debian /debootstrap/debootstrap --second-stage
 
-echo "LANG=pt_BR.UTF-8" > /mnt/sd/etc/locale.conf
-echo "KEYMAP=br-abnt2" > /mnt/sd/etc/vconsole.conf
-echo "en_US.UTF-8 UTF-8" > /mnt/sd/etc/locale.gen
-echo "pt_BR.UTF-8 UTF-8" >> /mnt/sd/etc/locale.gen
+echo "LANG=pt_BR.UTF-8" > /mnt/debian/etc/locale.conf
+echo "KEYMAP=br-abnt2" > /mnt/debian/etc/vconsole.conf
+echo "en_US.UTF-8 UTF-8" > /mnt/debian/etc/locale.gen
+echo "pt_BR.UTF-8 UTF-8" >> /mnt/debian/etc/locale.gen
 
-ln -sf /mnt/sd/usr/share/zoneinfo/America/Sao_Paulo /mnt/sd/etc/localtime
+ln -sf /mnt/debian/usr/share/zoneinfo/America/Sao_Paulo /mnt/debian/etc/localtime
 
-echo "$HOSTNAME" > /mnt/sd/etc/hostname
-echo "127.0.0.1 localhost.localdomain localhost" > /mnt/sd/etc/hosts
-echo "::1 localhost.localdomain localhost" >> /mnt/sd/etc/hosts
-echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /mnt/sd/etc/hosts
+echo "$HOSTNAME" > /mnt/debian/etc/hostname
+echo "127.0.0.1 localhost.localdomain localhost" > /mnt/debian/etc/hosts
+echo "::1 localhost.localdomain localhost" >> /mnt/debian/etc/hosts
+echo "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /mnt/debian/etc/hosts
 
-echo -e "PARTUUID=$ROOT_PARTUUID\t/\text4\tdefaults\t0\t0" > /mnt/sd/etc/fstab
+echo -e "PARTUUID=$ROOT_PARTUUID\t/\text4\tdefaults\t0\t0" > /mnt/debian/etc/fstab
 
-chroot /mnt/sd /bin/apt install -y bash udev sudo u-boot-tools parted initramfs-tools nano iwd network-manager openssh-server ntpdate iputils-ping wget curl dosfstools ntfs-3g xfsprogs e2fsprogs btrfs-progs tar zip unzip binutils build-essential cargo ffmpeg python3 python3-venv python3-pip git htop lm-sensors firmware-misc-nonfree firmware-atheros firmware-realtek debootstrap
+chroot /mnt/debian /bin/apt install -y bash udev sudo u-boot-tools parted initramfs-tools nano iwd network-manager openssh-server ntpdate iputils-ping wget curl dosfstools ntfs-3g xfsprogs e2fsprogs btrfs-progs tar zip unzip binutils build-essential cargo ffmpeg python3 python3-venv python3-pip git htop lm-sensors firmware-misc-nonfree firmware-atheros firmware-realtek debootstrap
 
-chroot /mnt/sd /bin/useradd -m -G sudo user
-chroot /mnt/sd /bin/passwd user
+chroot /mnt/debian /bin/useradd -m -G sudo user
+chroot /mnt/debian /bin/passwd user
 
-chroot /mnt/sd /bin/chsh -s /bin/bash
-chroot /mnt/sd /bin/chsh -s /bin/bash user
+chroot /mnt/debian /bin/chsh -s /bin/bash
+chroot /mnt/debian /bin/chsh -s /bin/bash user
